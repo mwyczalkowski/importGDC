6a1f1cb02cf827f2fa700e02e0107f852f8cf7d7
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2017-11-07 15:38:13 -0600

    Incomplete implementation of indexing on docker
    * Container script to download and index data
    * MGI and vanilla docker implementation

--- GDC_import.sh
+++ GDC_import.sh
@@ -1,19 +1,47 @@
-# Import GDC data using bsub and docker
-# Usage: GDC_import.sh UUID [UUID2 ...]
+# Launch docker instance to import and index GDC data
+# Usage: GDC_import.sh [options] UUID [UUID2 ...]
+#
+# -M: run in MGI environment
+# -D: output directory on localhost.  Mandatory
+# -t: token file path in container.  Mandatory
+# -n: filename associated with UUID.  Mandatory
+# -p: dataformat (BAM or FASTQ).  Mandatory
+# -d: dry run - print out docker statement but do not execute (for debugging)
+# -B: run bash instead of process_GDC_uuid.sh
 
-# TODO: Create wrapper script which will index this file, using command e.g., 
-# samtools index CPT0000110163.WholeExome.RP-1303.bam
+# This is run from the host computer.  
+# Executes script image.init/process_GDC_uuid.sh from within docker container
 
-# TODO: this needs to have BAM filename passed. Do parsing of JSON stuff here?
-
-# Using UUID instead
-TOKEN="import-config/gdc-user-token.2017-10-27T18-16-34.880Z.txt"
-
-
-OUTD="/gscmnt/gc2521/dinglab/mwyczalk/CPTAC3-download"
-echo Output to $OUTD
-
+# start process_GDC_uuid.sh in vanilla docker environment
 function processUUID {
+UUID=$1
+OUTD=$2
+TOKEN=$3
+FN=$4
+DF=$5
+
+# This starts mwyczalkowski/gdc-client and maps directories:
+# Container: /data
+# Host: $OUTD
+
+IMAGE="mwyczalkowski/gdc-client"
+
+if [ -z $RUNBASH ]; then
+CMD="process_GDC_uuid.sh $UUID $TOKEN $FN $DF"
+else
+CMD="/bin/bash"
+fi
+
+if [ -z $DRYRUN ]; then
+    docker run -v $OUTD:/data -it $IMAGE $CMD
+else
+    >&2 echo docker run -v $OUTD:/data -it $IMAGE $CMD
+fi
+
+}
+
+# start docker in MGI environment
+function processUUID_MGI {
 ID=$1
 
 # logs will be written to $SCRIPTD/bsub_run-step_$STEP.err, .out
@@ -27,16 +55,71 @@ echo Writing bsub logs to $OUTLOG and $ERRLOG
 # CMD is passed as argument to /usr/local/bin/gdc-client
     # This is defined in /Users/mwyczalk/src/docker/mgi/Dockerfile.gdc-client with 
     # ENTRYPOINT ["/usr/local/bin/gdc-client"]
-# Documentation: https://docs.gdc.cancer.gov/Data_Transfer_Tool/Users_Guide/Accessing_Built-in_Help/
-CMD="download -t $TOKEN -d $OUTD $ID"
+#CMD="download -t $TOKEN -d $OUTD $ID"
 
 bsub -q research-hpc $LOGS -a 'docker (mwyczalkowski/gdc-client)' "$CMD"
 
 }
 
+# http://wiki.bash-hackers.org/howto/getopts_tutorial
+while getopts ":Mt:D:p:n:dB" opt; do
+  case $opt in
+    M)  # example of binary argument
+      MGI=1
+      >&2 echo MGI Mode
+      ;;
+    t)
+      TOKEN=$OPTARG
+      >&2 echo Token file: $TOKEN
+      ;;
+    D)
+      OUTD=$OPTARG
+      >&2 echo Host data dir: $OUTD
+      ;;
+    p)
+      DF=$OPTARG
+      >&2 echo Data Format: $DF
+      ;;
+    n)
+      FN=$OPTARG
+      >&2 echo Filename: $FN
+      ;;
+    d)  # example of binary argument
+      DRYRUN=1
+      >&2 echo Dry run
+      ;;
+    B)  
+      RUNBASH=1
+      >&2 echo Run bash
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+if [ -z $TOKEN ]; then
+    >&2 echo Error: token not defined \[-t\]
+    exit
+fi
+if [ -z $OUTD ]; then
+    >&2 echo Error: output directory not defined \[-o\]
+    exit
+fi
+
 for UUID in "$@"
 do
 
-processUUID $UUID
+if [ $MGI ]; then
+processUUID_MGI $UUID
+else
+processUUID $UUID $OUTD $TOKEN $FN $DF
+fi
 
 done
---
+++ image.init/process_GDC_uuid.sh
@@ -0,0 +1,60 @@
+
+# Usage: process_GDC_uuid.sh [-O OUTD] UUID TOKEN FN DT
+# Download and index (if BAM) data from GDC
+# Arguments:
+#   UUID - UUID of object to download.  Mandatory
+#   TOKEN - token filename visible from container.  Mandatory
+#   FN - filename of object.  Required, used only for indexing
+#   DF - dataformat of object (BAM, FASTQ).  Required
+#   OUTD - base of imported data dir, visible from container.  Default is /data/GDC_import.  Optional
+
+OUTD="/data/GDC_import"
+
+# http://wiki.bash-hackers.org/howto/getopts_tutorial
+while getopts ":O:" opt; do
+  case $opt in
+#    M)  # example of binary argument
+#      MGI=1
+#      >&2 echo MGI Mode
+#      ;;
+    O)
+      OUTD=$OPTARG
+      >&2 echo Output dir: $OUTD
+      ;;
+    \?)
+      >&2 echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      >&2 echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+if [ "$#" -ne 4 ]
+then
+    >&2 echo "Error - invalid number of arguments"
+    >&2 echo Usage: process_GDC_uuid.sh [-O OUTD] UUID TOKEN FN DT
+    exit 1
+fi
+
+UUID=$1
+TOKEN=$2
+FN=$3
+DT=$4
+
+# Documentation of gdc-client: https://docs.gdc.cancer.gov/Data_Transfer_Tool/Users_Guide/Accessing_Built-in_Help/
+# GDC Client saves data to file $OUTD/$UUID/$FN.  We take advantage of this information to index BAM file after download
+/usr/local/bin/gdc-client download -t $TOKEN -d $OUTD $UUID
+
+if [ $DT == "BAM" ]; then
+DAT=$OUTD/$UUID/$FN
+>&2 Indexing $DAT
+
+/usr/local/bin/samtools index $DAT
+
+fi
+
+
