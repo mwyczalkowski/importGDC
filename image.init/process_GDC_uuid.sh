6b4f4e5ae776492d09e75ef7b20bb8df661b6a82
Matthew A. Wyczalkowski <m.wyczalkowski@gmail.com>
2017-11-07 23:18:31 +0000

    Improved functionality for downloading and indexing

--- image.init/process_GDC_uuid.sh
+++ image.init/process_GDC_uuid.sh
@@ -1,24 +1,27 @@
 
-# Usage: process_GDC_uuid.sh [-O OUTD] UUID TOKEN FN DT
+# Usage: process_GDC_uuid.sh [options] UUID TOKEN FN DT
 # Download and index (if BAM) data from GDC
 # Arguments:
 #   UUID - UUID of object to download.  Mandatory
 #   TOKEN - token filename visible from container.  Mandatory
 #   FN - filename of object.  Required, used only for indexing
 #   DF - dataformat of object (BAM, FASTQ).  Required
-#   OUTD - base of imported data dir, visible from container.  Default is /data/GDC_import.  Optional
+# Options:
+#   -O OUTD: base of imported data dir, visible from container.  Default is /data/GDC_import.  Optional
+#   -D: Download only, do not index
+#   -I: Index only, do not Download.  DT must be "BAM"
 
 OUTD="/data/GDC_import"
 
 # http://wiki.bash-hackers.org/howto/getopts_tutorial
-while getopts ":O:" opt; do
+while getopts ":O:DI" opt; do
   case $opt in
-#    M)  # example of binary argument
-#      MGI=1
-#      >&2 echo MGI Mode
-#      ;;
-    O)
-      OUTD=$OPTARG
+    D)  # Download only
+      DLO=1
+      >&2 echo MGI Mode
+      ;;
+    I)  # Index only
+      IXO=1
       >&2 echo Output dir: $OUTD
       ;;
     \?)
@@ -36,7 +39,7 @@ shift $((OPTIND-1))
 if [ "$#" -ne 4 ]
 then
     >&2 echo "Error - invalid number of arguments"
-    >&2 echo Usage: process_GDC_uuid.sh [-O OUTD] UUID TOKEN FN DT
+    >&2 echo Usage: process_GDC_uuid.sh \[options\] UUID TOKEN FN DT
     exit 1
 fi
 
@@ -47,13 +50,29 @@ TOKEN=$2
 FN=$3
 DT=$4
 
+# Where we expect output to go
+DAT=$OUTD/$UUID/$FN
+
+# Download if not "index only"
+
+if [ -z $IXO ]; then
+>&2 echo Writing to $DAT
+
 # Documentation of gdc-client: https://docs.gdc.cancer.gov/Data_Transfer_Tool/Users_Guide/Accessing_Built-in_Help/
 # GDC Client saves data to file $OUTD/$UUID/$FN.  We take advantage of this information to index BAM file after download
 /usr/local/bin/gdc-client download -t $TOKEN -d $OUTD $UUID
+fi
 
-if [ $DT == "BAM" ]; then
-DAT=$OUTD/$UUID/$FN
+
+## Now index if this is a BAM file, and not download-only
+if [ $DT == "BAM" ] && [ -z $DLO ] ; then
 >&2 echo Indexing $DAT
+
+# Confirm $DAT exists
+if [ ! -f $DAT ]; then
+>&2 echo BAM file $DAT does not exist.  Not indexing.
+exit
+fi
 
 /usr/bin/samtools index $DAT
 
